b5bdce541b319ea2b4c8acb5389ec77058ee30c7
Hugo Serodio <hugo.serodio@maxiv.lu.se>
2021-09-12 09:49:19 +0200

    added sql db

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap  
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager, login_user, logout_user
+from flask_login.mixins import UserMixin
+
+
+
 from wtforms import StringField, PasswordField
 from wtforms.validators import Length, Email
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "THIS IS A SECRET"
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlite.db"
 
 Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager()
+login.init_app(app)
 
+@login.user_loader
+def user_loader(user_id):
+    return User.query.filter_by(id=user_id).first()
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
 
 
 class loginForm(FlaskForm):
@@ -34,19 +58,36 @@ def login():
     form = loginForm()
 
     if form.validate_on_submit():
-        return "it's valid"
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if check_password_hash(user.password,form.password.data):
+            login_user(user)
+
+            return redirect(url_for("index"))
+
     return render_template("login.html", form=form)
+
 
 @app.route("/register",  methods=["GET","POST"])
 def register():
     form = registerForm()
 
-    if form.validate_on_submit():
-        return "it's valid"
+    if form.validate_on_submit() and form.password.data == form.repeat_password.data:
+        user = User(
+            email = form.email.data,
+            password = generate_password_hash(form.password.data)
+        )
+        db.session.add(user)
+        db.session.commit()
+    
+        return redirect(url_for("index"))
 
     return render_template("register.html", form=form)
 
-
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
 
 if __name__=="__main__":
     app.run()
